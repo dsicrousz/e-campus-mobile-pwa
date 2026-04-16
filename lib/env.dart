51b8b60153365673f0646640067b0900d313c23e
dsicrousz <dsicrousz@gmail.com>
2026-04-16 12:13:13 +0000

    fix url

--- lib/env.dart
+++ lib/env.dart
@@ -1,2 +1,3 @@
-String backUrl = "http://localhost:3001";
+// String backUrl = "http://localhost:3001";
+String backUrl = "https://ecampusapi.crousz.com";
 String profilUrl = "https://studentback.crousz.app";
